b0a986c6d62eb4474f760ed288f6826be4bfd28b
Rashed Abdel-Tawab <rashed@linux.com>
2018-12-26 19:35:49 -0500

    Add 802.11ac to IO80211Controller and add original Apple copyright
    This is pretty much a no-op, this enum isn't even used, but w/e

--- IntelWifi/IntelWifi/apple80211/IO80211Controller.h
+++ IntelWifi/IntelWifi/apple80211/IO80211Controller.h
@@ -1,3 +1,25 @@
+/*
+ * Copyright (c) 2005 Apple Computer, Inc. All rights reserved.
+ *
+ * @APPLE_LICENSE_HEADER_START@
+ *
+ * The contents of this file constitute Original Code as defined in and
+ * are subject to the Apple Public Source License Version 1.1 (the
+ * "License").  You may not use this file except in compliance with the
+ * License.  Please obtain a copy of the License at
+ * http://www.apple.com/publicsource and read it before using this file.
+ *
+ * This Original Code and all software distributed under the License are
+ * distributed on an "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, EITHER
+ * EXPRESS OR IMPLIED, AND APPLE HEREBY DISCLAIMS ALL SUCH WARRANTIES,
+ * INCLUDING WITHOUT LIMITATION, ANY WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE OR NON-INFRINGEMENT.  Please see the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * @APPLE_LICENSE_HEADER_END@
+ */
+
 #ifndef _IO80211CONTROLLER_H
 #define _IO80211CONTROLLER_H
 
@@ -26,12 +48,15 @@
  @constant LINK_SPEED_80211A 54 Mbps
  @constant LINK_SPEED_80211B 11 Mbps.
  @constant LINK_SPEED_80211G 54 Mbps.
+ @constant LINK_SPEED_80211N 300 Mbps.
+ @constant LINK_SPEED_80211AC 1733.3 Mbps.
  */
 enum {
     LINK_SPEED_80211A    = 54000000ul,        // 54 Mbps
     LINK_SPEED_80211B    = 11000000ul,        // 11 Mbps
     LINK_SPEED_80211G    = 54000000ul,        // 54 Mbps
-    LINK_SPEED_80211N    = 300000000ul,        // 300 Mbps (MCS index 15, 400ns GI, 40 MHz channel)
+    LINK_SPEED_80211N    = 300000000ul,       // 300 Mbps (MCS index 15, 400ns GI, 40 MHz channel)
+    LINK_SPEED_80211AC   = 1733000000ul,      // 1733.3 Mbps
 };
 
 enum IO80211CountryCodeOp
